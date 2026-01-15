9b8491e272132c192f788bdcf87ad4caa7f5fe2e
will <will@256k1.dev>
2026-01-15 21:18:57 +0000

    use commit hash in build name

--- scripts/guix.cmake
+++ scripts/guix.cmake
@@ -1,11 +1,27 @@
 cmake_host_system_information(RESULT OS_PLATFORM QUERY OS_PLATFORM)
 
 set(CTEST_SITE "${OS_PLATFORM}-nixos-ryzen5")
-set(CTEST_BUILD_NAME "Guix")
 set(CTEST_SOURCE_DIRECTORY "/data/bitcoin")
 set(CTEST_BINARY_DIRECTORY "/data/bitcoin")
 
-set(CTEST_BUILD_COMMAND "bash -c \"unset SOURCE_DATE_EPOCH && git fetch origin && git reset --hard origin/master && ${CTEST_SOURCE_DIRECTORY}/contrib/guix/guix-build\"")
+# Fetch, reset, then get commit hash for build name
+execute_process(
+  COMMAND git fetch origin
+  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+)
+execute_process(
+  COMMAND git reset --hard origin/master
+  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+)
+execute_process(
+  COMMAND git rev-parse --short=12 HEAD
+  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+  OUTPUT_VARIABLE GIT_REV
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+set(CTEST_BUILD_NAME "Guix-${GIT_REV}")
+
+set(CTEST_BUILD_COMMAND "bash -c \"unset SOURCE_DATE_EPOCH && ${CTEST_SOURCE_DIRECTORY}/contrib/guix/guix-build\"")
 
 file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}/Testing")
 
