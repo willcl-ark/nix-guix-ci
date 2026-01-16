dcf671780180638826109a3b4e851e414f5a1109
will <will@256k1.dev>
2026-01-16 15:35:11 +0000

    don't rebuild same commit multiple times

--- scripts/guix.cmake
+++ scripts/guix.cmake
@@ -4,38 +4,51 @@ set(CTEST_SITE "${OS_PLATFORM}-nixos-ryzen5")
 set(CTEST_SOURCE_DIRECTORY "/data/bitcoin")
 set(CTEST_BINARY_DIRECTORY "/data/bitcoin")
 
-# Fetch, reset, then get commit hash for build name
-# We do this manually rather than with ctest_update() as we want to set
+# We fetch manually rather than with ctest_update() as we want to set
 # CTEST_BUILD_NAME dynamically, but ctest_update() must run after
 # ctest_start(), which needs the name set
 execute_process(
-  COMMAND git fetch origin
+  COMMAND git rev-parse HEAD
   WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
-)
-execute_process(
-  COMMAND git reset --hard origin/master
-  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
-)
-execute_process(
-  COMMAND git rev-parse --short=12 HEAD
-  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
-  OUTPUT_VARIABLE GIT_REV
+  OUTPUT_VARIABLE OLD_HEAD
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+# Poll until a new commit is available
+while(TRUE)
+  execute_process(
+    COMMAND git fetch origin
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+  )
+  execute_process(
+    COMMAND git rev-parse origin/master
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+    OUTPUT_VARIABLE NEW_HEAD
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if(NOT OLD_HEAD STREQUAL NEW_HEAD)
+    break()
+  endif()
+  message("No new commits (at ${OLD_HEAD}), sleeping 60s")
+  execute_process(COMMAND sleep 60)
+endwhile()
+
+execute_process(
+  COMMAND git reset --hard ${NEW_HEAD}
+  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+)
+string(SUBSTRING "${NEW_HEAD}" 0 12 GIT_REV)
 set(CTEST_BUILD_NAME "Guix-${GIT_REV}")
 
 set(CTEST_BUILD_COMMAND "bash -c \"unset SOURCE_DATE_EPOCH && ${CTEST_SOURCE_DIRECTORY}/contrib/guix/guix-build\"")
 
 file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}/Testing")
 
-# Make sure we are clean
 execute_process(
   COMMAND git clean -dfx --exclude=CTestConfig.cmake
   WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
 )
 
 ctest_start("Continuous")
-
 ctest_build()
 
 # Add guix hashes as a note
