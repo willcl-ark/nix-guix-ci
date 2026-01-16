ff52b35ab7ede474fa2aa283c29f3e7c6360a957
will <will@256k1.dev>
2026-01-16 11:15:29 +0000

    upload hashes as notes properly

--- scripts/guix.cmake
+++ scripts/guix.cmake
@@ -38,12 +38,13 @@ ctest_start("Continuous")
 
 ctest_build()
 
-# Upload guix hashes as an artifact
+# Add guix hashes as a note
+file(MAKE_DIRECTORY "${CTEST_BINARY_DIRECTORY}/Testing/Notes")
 execute_process(
   COMMAND bash -c "
     REV=\$(git rev-parse --short=12 HEAD)
     BUILD_DIR=\"guix-build-\${REV}\"
-    HASH_FILE=\"${CTEST_BINARY_DIRECTORY}/Testing/build-hashes.txt\"
+    HASH_FILE=\"${CTEST_BINARY_DIRECTORY}/Testing/Notes/build-hashes.txt\"
     if [ -d \"\${BUILD_DIR}/output\" ]; then
       uname -m > \"\${HASH_FILE}\"
       find \"\${BUILD_DIR}/output/\" -type f -print0 | env LC_ALL=C sort -z | xargs -r0 sha256sum >> \"\${HASH_FILE}\"
@@ -51,8 +52,5 @@ execute_process(
   "
   WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
 )
-if(EXISTS "${CTEST_BINARY_DIRECTORY}/Testing/build-hashes.txt")
-  ctest_upload(FILES "${CTEST_BINARY_DIRECTORY}/Testing/build-hashes.txt")
-endif()
 
 ctest_submit()
