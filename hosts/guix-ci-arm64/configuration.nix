3ab732e70fb34c705b5f130620579519f80b62a8
will <will@256k1.dev>
2026-01-19 09:54:37 +0000

    Write startup.nsh backup file
    Without this we probably fail to boot on our prevps arm64 host.
    
    TODO: Find a more rboust solution than this; this slows down reboot
    times considerably :(

--- hosts/guix-ci-arm64/configuration.nix
+++ hosts/guix-ci-arm64/configuration.nix
@@ -8,4 +8,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.efi.efiSysMountPoint = "/boot";
+
+  # UEFI auto-detect fails on this host, so we use startup.nsh as fallback.
+  # UEFI shell executes startup.nsh automatically when other boot options fail.
+  systemd.tmpfiles.rules = [
+    "f /boot/startup.nsh 0644 root root - FS0:\\EFI\\BOOT\\BOOTAA64.EFI"
+  ];
 }
